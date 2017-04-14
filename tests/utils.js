f99e26344760d6a2a05fa483e0143231e4b9e4f1
Giacomo Triggiano <giacomotriggiano@gmail.com>
2017-04-14 09:50:42 +0200

    ..

--- tests/utils.js
+++ tests/utils.js
@@ -8,13 +8,13 @@ const { DefineError, schemaValidator } = require(`${libFolder}/utils`)
 describe('Utils', function () {
   describe('DefineError(errorName)', function () {
     it('is a function', () => should(DefineError).be.a.Function())
-    describe(`CustomError(message, extra) = DefineError(errorName)`, () => {
+    describe(`CustomError(message[, extra]) = DefineError(errorName)`, () => {
       it('is a function', () => should(DefineError('myerror')).be.a.Function())
       it('CustomError.name === errorName', () => {
         let CustomError = DefineError('myerror')
         should(CustomError.name).equal('myerror')
       })
-      describe('e = CustomError(message, extra)', () => {
+      describe('e = CustomError(message[, extra])', () => {
         it('is an instance of Error', () => {
           let CustomError = DefineError('myerror')
           let e = new CustomError()
@@ -24,6 +24,11 @@ describe('Utils', function () {
           let CustomError = DefineError('myerror')
           let e = new CustomError('my error message')
           should(e.message).equal('my error message')
+        })
+        it('e.extra is undefined if not provided', () => {
+          let CustomError = DefineError('myerror')
+          let e = new CustomError('my error message')
+          should(e.extra).be.undefined()
         })
         it('e.extra === extra', () => {
           let extra = {}
